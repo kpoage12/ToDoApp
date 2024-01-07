bc5bf04131fbb052bd06db8da9325d14e0be40ae
Kyle Poage <kylepoage2003@gmail.com>
2024-01-06 17:04:32 -0800

    Adding files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TodoWrapper from "./components/TodoWraper";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper />
+    </div>
+  );
+}
+
+export default App;
